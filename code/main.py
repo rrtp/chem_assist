5ab799afbc48248073fbd35473f9e6300e03e906
rrtp <pganeshrp@gmail.com>
2024-10-14 08:27:55 +0530

    linked welcome page,main_menu,reactions, quiz button

--- code/main.py
+++ code/main.py
@@ -22,7 +22,7 @@ class Application(Gtk.Application):
         win.present()
         print(win.get_css_name())
 
-
+#welcome window
 class welcome_window(Gtk.ApplicationWindow):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
@@ -31,27 +31,68 @@ class welcome_window(Gtk.ApplicationWindow):
         self.set_title("Welcome!")
 
         #create welcome text
-        welcome_text=Gtk.Label.new("Welcome to Chemistry Assistant!")
-        self.set_child(welcome_text)
+        welcome_button=Gtk.Button.new_with_label("Welcome to Chemistry Assistant!")
+        self.set_child(welcome_button)
+        welcome_button.connect('clicked',self.on_click)
 
+    def on_click(self,button):
+        self.close()
+        win=main_menu_window(application=app)
+        win.present()
+
+#fallback class if no obj arg is present
+class fallback():
+    def get_label():
+        return "no button"
+#close current window
+def close_win(obj=fallback,*args):
+    print(obj.get_label()," button selected, exiting..")
+    app.get_active_window().close()
+#open quiz window
+def open_quiz_main_page_win(*args,**kwargs):
+    app.get_active_window().close()
+    win=quiz_main_page_win(application=app)
+    win.present()
+#open reactions page
+def open_reactions_display_win(*args,**kwargs):
+    app.get_active_window().close()
+    win=reactions_display_win(application=app)
+
+#main menu window
 class main_menu_window(Gtk.ApplicationWindow):
-    def __init__():
-        super().init()
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
 
         self.set_title("Chemistry assistant main page")
 
         #buttons
         reactions_button=Gtk.Button.new_with_label("Reactions")
-        self.set_child(reactions_button)
         quiz_button=Gtk.Button.new_with_label("Quiz")
-        self.set_child(quiz_button)
+        quit_button=Gtk.Button.new_with_label("Quit")
 
         #main menu buttons box
-        main_menu_buttons_box=Gtk.Box.new(Gtk.orientation.VERTICAL,0)
+        main_menu_buttons_box=Gtk.Box.new(Gtk.Orientation.VERTICAL,0)
         main_menu_buttons_box.append(reactions_button)
         main_menu_buttons_box.append(quiz_button)
+        main_menu_buttons_box.append(quit_button)
         self.set_child(main_menu_buttons_box)
 
+        #button functions
+        reactions_button.connect('clicked',open_reactions_display_win)
+        quiz_button.connect('clicked',open_quiz_main_page_win)
+        quit_button.connect('clicked',close_win)
+
+class reactions_display_win(Gtk.ApplicationWindow):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+        self.set_title("Reactions")
+
+class quiz_main_page_win(Gtk.ApplicationWindow):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+    
+        self.set_title("Quiz main page")
 #Create an instance of Application
 app=Application()
 app.run(None)
