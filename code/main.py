b56756be1b64c47b4e097d441bd0b183f31c242e
rrtp <pganeshrp@gmail.com>
2024-10-19 02:22:18 +0530

    moved db,css,ui directory variables to pages.py

--- code/main.py
+++ code/main.py
@@ -3,22 +3,11 @@ gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk
 import pages
 
-#folder for Css, user interface layout definition files
-css_dir="styles/"
-ui_dir="ui_definitions/"
-db_dir="databases/"
-
-#css load
-css_file_path=css_dir+"styles.css"
-css_provider=Gtk.CssProvider.new()
-css_provider.load_from_path(css_file_path)
-
 #custom gtk application class containing helpful definitions
 class Application(Gtk.Application):
     def __init__(self):
         super().__init__(application_id="com.chem_assist_project.chem_assist")
 
-        menu_bar=Gtk.MenuModel.new("Actions",)
         self.connect('activate',self.on_activate)
     def on_activate(self,app):
         print("activated")
